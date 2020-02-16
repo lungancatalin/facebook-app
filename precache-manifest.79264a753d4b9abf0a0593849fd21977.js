29b80915edac12fb3617a1932a3f3da62d0b580e
Catalin <lungancatalin@yahoo.com>
2020-02-16 22:22:28 +0200

    Updates

---
+++ precache-manifest.79264a753d4b9abf0a0593849fd21977.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d40f5155332e3005d83b1a93fa4774b4",
+    "url": "/facebook_app/index.html"
+  },
+  {
+    "revision": "ed48352933ff6955b09b",
+    "url": "/facebook_app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "802b9f547e9ff2202de6",
+    "url": "/facebook_app/static/js/2.a7df3a29.chunk.js"
+  },
+  {
+    "revision": "7f180084957a789defed63ce6af6bf93",
+    "url": "/facebook_app/static/js/2.a7df3a29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed48352933ff6955b09b",
+    "url": "/facebook_app/static/js/main.74d33ee3.chunk.js"
+  },
+  {
+    "revision": "79787adef0466a22a709",
+    "url": "/facebook_app/static/js/runtime-main.4333aeb6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/facebook_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
