ab1bdfaf60f05d05af66099356257cfcf071c3e9
Catalin <lungancatalin@yahoo.com>
2020-02-16 22:37:06 +0200

    Updates

---
+++ precache-manifest.54bb6f07e683929f63efa97236d75a4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa9314724bdede4082d61d6f1310daf6",
+    "url": "/facebook-app/index.html"
+  },
+  {
+    "revision": "2cd51e6e467afe21f3be",
+    "url": "/facebook-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "802b9f547e9ff2202de6",
+    "url": "/facebook-app/static/js/2.a7df3a29.chunk.js"
+  },
+  {
+    "revision": "7f180084957a789defed63ce6af6bf93",
+    "url": "/facebook-app/static/js/2.a7df3a29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cd51e6e467afe21f3be",
+    "url": "/facebook-app/static/js/main.a2a279c6.chunk.js"
+  },
+  {
+    "revision": "d7b0f2ad2a49359806a7",
+    "url": "/facebook-app/static/js/runtime-main.a6771948.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/facebook-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/facebook_app/precache-manifest.79264a753d4b9abf0a0593849fd21977.js"
+  "/facebook-app/precache-manifest.54bb6f07e683929f63efa97236d75a4d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/facebook_app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/facebook-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
